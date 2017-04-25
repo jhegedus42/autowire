9dbfc7f8e72eda078909260261418094921743ee
Jozsef Hegedus <jhegedus42@gmail.com>
2017-04-25 11:04:54 +0200

    simplified code

--- autowire/shared/src/test/scala/autowire/UpickleTests.scala
+++ autowire/shared/src/test/scala/autowire/UpickleTests.scala
@@ -1,10 +1,12 @@
 package autowire
 
+import java.io.Writer
+
 import utest._
 import utest.framework._
 import utest.framework.ExecutionContext.RunNow
-import upickle.Js
-import upickle.default._
+//import upickle.Js
+//import upickle.default._
 import acyclic.file
 
 import scala.concurrent.Future
@@ -13,54 +15,48 @@ object Test extends TestSuite {
 
   val tests = TestSuite {
     'example {
-      import upickle._
 
-      // shared API interface
-      trait MyApi {
-        def doThing(i: Int, s: String): Seq[String]
-//        val subApiString:SubApi[String]
+      trait MyApi[T] {
+        val subApiString: SubApi[T]
       }
 
-      trait SubApi[T]{
-        def fooFighter(s:T):String
+      trait SubApi[T] {
+        def fooFighter(s: T): String // does not compile
+//        def fooFighterString(s:String): String // compiles fine
       }
 
       // server-side implementation, and router
-      object MyApiImpl extends MyApi {
-        def doThing(integerInput: Int, stringInput: String) = Seq.fill(integerInput)(stringInput)
+      object MyApiImpl extends MyApi[String] {
 
-//        override val subApiString: SubApi[String] =  null
+        override val subApiString: SubApi[String] = null
       }
 
-      object MyServer
-          extends autowire.Server[String,
-                                  upickle.default.Reader,
-                                  upickle.default.Writer] {
-        def write[Result: Writer](r: Result) = upickle.default.write(r)
+      trait MyReader[T] {}
 
-        def read[Result: Reader](p: String) = upickle.default.read[Result](p)
-//        def read[Result: Reader](p: String) = ???
+      implicit val mr: MyReader[String] = ???
+      implicit val mi: MyReader[Int] = ???
 
-//        case class Request[PickleType](path : Seq[String], args: Map[String, PickleType])
-        val routes:PartialFunction[Core.Request[String], Future[String]] = MyServer.route[MyApi](MyApiImpl)
+      implicit val msw: MyWriter[Seq[String]] = ???
+      implicit val mwsw: MyWriter[String] = ???
+
+      trait MyWriter[T] {}
+
+      // we serialize into String
+      trait MyServer[T] extends autowire.Server[String, MyReader, MyWriter] {
+        def write[Result: MyWriter](r: Result): String = ???
+        // upickle.default.write(r)
+
+        def read[Result: MyReader](p: String): Result = ???
+
+//        implicit val iv : MyReader[T] = ???
       }
 
-//      // client-side implementation, and call-site
-//      object MyClient
-//          extends autowire.Client[String,
-//                                  upickle.default.Reader,
-//                                  upickle.default.Writer] {
-//        def write[Result: Writer](r: Result) = upickle.default.write(r)
-//
-//        def read[Result: Reader](p: String) = upickle.default.read[Result](p)
-//
-//        override def doCall(req: Request) = {
-//          println(req)
-//          MyServer.routes.apply(req)
-//        }
-//      }
-//
-//      MyClient[MyApi].doThing(3, "lol").call().foreach(println)
+      object MyServerObj extends MyServer[String]{
+
+        val routes: PartialFunction[Core.Request[String], Future[String]] = MyServerObj.route[MyApi[String]](MyApiImpl)
+      }
+
+
     }
 
   }
